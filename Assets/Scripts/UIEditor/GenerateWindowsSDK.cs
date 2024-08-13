4eeccc2d919c2cd6135ab5dd0e068236cdfbc366
alfredojuareztamayo <juareztamayojuanalfredo@gmail.com>
2024-08-12 23:58:23 -0600

    fix editor problem

--- Assets/Scripts/UIEditor/GenerateWindowsSDK.cs
+++ Assets/Scripts/UIEditor/GenerateWindowsSDK.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class GenerateWindowsSDK : EditorWindow
 {
-    [MenuItem("Component/SDK_Sleekhell")]
+    //[MenuItem("Component/SDK_Sleekhell")]
     //[MenuItem("Component/SDK_Sleekhell/Platforms")]
    
     // Start is called before the first frame update
@@ -47,5 +47,52 @@ public class GenerateWindowsSDK : EditorWindow
             }
         }
         return false;
+    }
+    //[MenuItem("Component/SDK_Sleekhell/Platforms")]
+    [MenuItem("Component/SDK_Sleekhell/Platforms/AtoBPrefab")]
+    private static void CreateAtoBPlatform()
+    {
+        GameObject selecte = Selection.activeGameObject;
+        if (selecte != null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefab/Platform/PlatformAtoB/AtoBnoTime/PlatformAtoB");
+            GameObject prefab2 = Resources.Load<GameObject>("Prefab/Platform/PlatformAtoB/AtoBnoTime/PathsAtoB");
+            if (prefab2 != null && prefab !=null)
+            {
+                GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(prefab, selecte.transform);
+                instance.name = prefab.name; // Opcional: asegura que el nombre del objeto instanciado sea igual al prefab
+                Undo.RegisterCreatedObjectUndo(instance, "Create AtoB Platform");
+                GameObject instance2 = (GameObject)PrefabUtility.InstantiatePrefab(prefab2, selecte.transform);
+                instance2.name = prefab2.name; // Opcional: asegura que el nombre del objeto instanciado sea igual al prefab
+                Undo.RegisterCreatedObjectUndo(instance2, "Create AtoB Platform Path");
+
+            }
+            else
+            {
+                Debug.LogError("<color=blue>Prefab 'AtoB' no encontrado en Resources/Prefabs/atob.</color>");
+            }
+        }
+        else
+        {
+            Debug.Log("<color=green>No GameObject Selected</color>");
+        }
+    }
+
+    [MenuItem("Component/SDK_Sleekhell/Platforms/AtoB")]
+    private static void CreateAtoBPlatformScript()
+    {
+        GameObject selected = Selection.activeGameObject;
+        if (selected)
+        {
+            if(selected.GetComponent<BoxCollider>() == null)
+            {
+                Undo.AddComponent<BoxCollider>(selected);
+            }
+           Undo.AddComponent<PlatformPathsAtoB>(selected);
+        }
+        else
+        {
+            Debug.Log("<color=green>No GameObject Selected</color>");
+        }
     }
 }
